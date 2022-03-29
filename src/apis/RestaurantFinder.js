a9f86d8307de4fab8fd884d10fe5856a552b5ff6
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-03-29 20:27:18 +0530

    changed base url

--- src/apis/RestaurantFinder.js
+++ src/apis/RestaurantFinder.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000/api/v1/restaurants",
+  // baseURL: "http://localhost:4000/api/v1/restaurants",
+  baseURL:
+    "https://avi-restaurant-reviews-backend.herokuapp.com/api/v1/restaurants",
 });
